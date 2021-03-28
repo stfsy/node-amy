503d344b30944204194b3474673ed2b9065ff22c
stfsy <s.pfaffel@gmail.com>
2021-03-28 22:28:18 +0200

    refactor: rename file and use es6 classes

--- test/spec/hashing/text-node-hasher.spec.js
+++ test/spec/hashing/text-node-hasher.spec.js
@@ -3,11 +3,13 @@
 const resolve = require('path').resolve
 
 const Node = require('node-html-light').Node
-const hasher = require(resolve('lib/hashing/256-hasher'))
+const Hasher = require(resolve('lib/hashing/text-node-hasher'))
+const hasher = new Hasher({ fn: 'sha256' })
+
 const expect = require('chai').expect
 
 describe('Hasher', () => {
-    it('computes hash values correctly', () => {
+    it('computes sha-256 hash values correctly', () => {
         const node = Node.fromString(`((window) => {
             const updateHeight = () => {
                 const vh = window.innerHeight * 0.01
@@ -17,7 +19,7 @@ describe('Hasher', () => {
             updateHeight()
         })(window)`)
 
-        const hash = hasher(node)
+        const hash = hasher.hash(node, 'sha256')
         expect(hash).to.equal('sha256-JZg4LC51vCNw5SVZP/6uYntJ5Un6gpl7wmQIBCnjTX0=')
     })
 })
