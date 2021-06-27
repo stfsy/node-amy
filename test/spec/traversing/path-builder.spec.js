5735e6c11d59b5b06e06c8fbe451a53175dc42a7
stfsy <s.pfaffel@gmail.com>
2021-06-27 15:27:19 +0200

    feat: add path builder

---
+++ test/spec/traversing/path-builder.spec.js
@@ -0,0 +1,50 @@
+'use strict'
+
+const Node = require('node-html-light').Node
+const resolve = require('path').resolve
+const PathBuilder = require(resolve('lib/traversing/path-builder'))
+
+const expect = require('chai').expect
+
+describe.only('PathBuilder', () => {
+
+    const node = Node.fromString('<div><p>{{ id }}</p><div><span name="{{ user.name.first }}"></span><ul></ul></div></div>')
+
+    let pathBuilder
+
+    beforeEach(() => {
+        pathBuilder = new PathBuilder()
+    })
+
+    it('does return empty array if element is root', () => {
+
+    })
+
+    it('returns an array for nested elements on level1', () => {
+        const paragraph = node.find('p')
+        const path = pathBuilder.buildPathFromParentToNode(paragraph[0])
+        expect(path).to.have.length(1)
+        expect(path[0]).to.equal(0)
+    })
+
+    it('returns an array for nested elements on level 2', () => {
+        const span = node.find('span')
+        const path = pathBuilder.buildPathFromParentToNode(span[0])
+        expect(path).to.deep.equal([1, 0])
+    })
+
+    it('returns an array with reversed indices', () => {
+        const list = node.find('ul')
+        const path = pathBuilder.buildPathFromParentToNode(list[0])
+        expect(path).to.deep.equal([1, 1])
+    })
+
+    it('return a path allows traversing back to target node', () => {
+        const list = node.find('ul')
+        const path = pathBuilder.buildPathFromParentToNode(list[0])
+        const target = path.reduce((parent, index) => {
+            return parent.children[index]
+        }, node.get())
+        expect(target.name).to.equal('ul')
+    })
+})
