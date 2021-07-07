f926a2eedc9cc2287edae329a29d8d41980f5fde
stfsy <s.pfaffel@gmail.com>
2021-07-07 19:28:47 +0200

    chore: add test component files

---
+++ test/fixtures/components/test-component-no-render-function.component.js
@@ -0,0 +1,5 @@
+module.exports = {
+    template: () => `
+            <button type="button" title="small blue diamond" aria-label="small blue diamond" class="tweet-emoji col-span-1 bg-white text-gray-600 hover:text-gray-800 px-3 py-2 font-medium text-base rounded-md hover:bg-gray-100 hover:shadow"></button>
+            `
+}
